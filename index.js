09e6b261e1839f4ffc6d61527f9bf88b87915f0d
hyesech <hyesech@gmail.com>
2021-03-15 18:29:27 +0900

    + loop(): 한 칸 아래 탐색 후 Flag 변경 코드 작성 + loop(): Flag 코드 구현
    + 움직이지 않는 block 색상 ---> red 설정
    + block의 배열 2x2 ---> 2x3
    + currentPosition 시작 값 -1로 변경

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const ROWS = 10; // 가로
 
 // 게임 상수 선언
 const tetris = document.querySelector("#tetris");
-const startPosition = [0, 4];
 const blockData = {
-  name: "block",
-  color: "blue",
   shape: [
+    [0, 0],
     [1, 1],
     [1, 1],
   ],
@@ -25,11 +23,11 @@ var currentPosition = []; // 변경된다.
 // INIT: 게임 초기화
 const init = () => {
   const fragment = document.createDocumentFragment();
-  [...Array(COLS).keys()].forEach((cor, i) => {
+  [...Array(COLS).keys()].forEach(() => {
     const tr = document.createElement("tr");
     fragment.appendChild(tr);
 
-    [...Array(ROWS).keys()].forEach((row, j) => {
+    [...Array(ROWS).keys()].forEach(() => {
       const td = document.createElement("td");
       tr.appendChild(td);
     });
@@ -45,12 +43,12 @@ const init = () => {
 const create = () => {
   console.log("FUNC: CREATE");
 
-  // 블록 스폰 위치: 상수값 복사 ? 이부분 필요할까?
-  currentPosition = startPosition;
+  // 블록 스폰 위치: 가상의 맨 윗줄이 있는 것처럼 연산
+  currentPosition = [-1, 4];
 
-  // 블록 데이터 생성
+  // 블록 데이터 생성: 맨 윗줄을 삭제하고 연산한다.
   const block = blockData.shape;
-  block.forEach((col, i) => {
+  block.slice(1).forEach((col, i) => {
     col.forEach((row, j) => {
       tetrisData[i][j + 4] = row;
     });
@@ -60,7 +58,7 @@ const create = () => {
   draw();
 };
 
-// LOOP: 1초마다 반복
+// LOOP: 1초마다 반복하며 블록을 한 칸 아래로 이동
 const loop = () => {
   console.log("FUNC: LOOP");
 
@@ -68,44 +66,62 @@ const loop = () => {
   const block = blockData.shape;
 
   // 움직이고 있는 Block의 좌표값을 담을 배열
-  const movingCells = [];
+  const movingBlocks = [];
 
   // 다음 위치 설정
   const nextPosition = [currentPosition[0] + 1, currentPosition[1]];
 
-  // FLAG: 이동 가능(true), 이동 불가(false)
+  // FLAG: 기본값 true(이동 가능)
   let canMove = true;
 
-  // 현재 위치에서 한 칸 아래로 이동
+  // 현재 위치에서 한 칸 아래를 탐색
+  console.log(currentPosition[0], currentPosition[1]);
   for (let i = currentPosition[0]; i < currentPosition[0] + block.length; i++) {
+    if (i < 0 || i >= 20) continue; // 탐색하는 tr의 범위를 0-19로 한정
     for (
       let j = currentPosition[1];
       j < currentPosition[1] + block.length;
       j++
     ) {
-      console.log(i, j);
+      if (tetrisData[i][j] === 1) {
+        // 움직일 수 있는 Block인 경우
+        movingBlocks.push([i, j]);
+        if (tetrisData[i + 1] === undefined || tetrisData[i + 1][j] === 2) {
+          // 움직일 수 없는 Block인 경우: 아래에 블럭이 있거나 최하단인 경우
+          canMove = false;
+        }
+      }
     }
   }
 
   // FLAG 상태에 따라 작업 분기
-  if (!canMove) {
+  if (canMove) {
+    console.log("이동 가능");
+    console.log(movingBlocks);
+
+    // tetrisData 배열에 현재 블록 위치 업데이트
+    for (let i = tetrisData.length - 1; i > 0; i--) {
+      const tr = tetrisData[i];
+      tr.forEach((td, j) => {
+        if (td === 1 && tetrisData[i + 1] && tetrisData[i + 1][j] !== 2) {
+          tetrisData[i + 1][j] = td;
+          tetrisData[i][j] = 0;
+        }
+      });
+    }
+    // 매 loop마다 반복해야 하는 작업
+    currentPosition = nextPosition;
+    draw();
+  } else if (!canMove) {
     console.log("이동불가");
+    movingBlocks.forEach((cell) => {
+      tetrisData[cell[0]][cell[1]] = 2;
+    });
 
     // 줄 삭제 가능한지 체크: rows가 전부 1인 경우
     checkRows();
     // 다음 위치에 새 블록 생성
-    // create();
-  } else if (canMove) {
-    console.log("이동 가능");
-
-    // 매 loop마다 반복해야 하는 작업
-    currentPosition = nextPosition;
-
-    console.log("--------");
-    console.log(tetrisData);
-    console.log("--------");
-
-    draw();
+    create();
   }
 };
 
@@ -115,12 +131,16 @@ const loop = () => {
 const draw = () => {
   console.log("func: draw");
 
-  tetrisData.forEach((col, i) => {
-    col.forEach((row, j) => {
-      if (row === 1) {
-        tetris.children[i].children[j].className = "blue";
-      } else {
+  tetrisData.forEach((tr, i) => {
+    tr.forEach((td, j) => {
+      if (td === 0) {
         tetris.children[i].children[j].className = "white";
+      } else if (td === 1) {
+        tetris.children[i].children[j].className = "blue";
+      } else if (td === 2) {
+        tetris.children[i].children[j].className = "red";
+      } else {
+        console.log("문제있음.");
       }
     });
   });
@@ -138,4 +158,4 @@ init();
 
 create();
 
-setInterval(loop, 1000);
+setInterval(loop, 500);
